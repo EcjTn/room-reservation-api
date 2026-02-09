fe403f2a378ba404ce7d70a89b9c6b483f0c8bbf
EcjTn <ecjtaneo@gmail.com>
2026-02-10 07:49:29 +0800

    added room queries ready for admins

---
+++ src/main/java/com/ecjtaneo/hotel_management_system/room/RoomRepository.java
@@ -0,0 +1,13 @@
+package com.ecjtaneo.hotel_management_system.room;
+
+import com.ecjtaneo.hotel_management_system.room.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    public Optional<Room> findByRoomNumber(String roomNumber);
+    public void deleteByRoomNumber(String roomNumber);
+}
--- src/main/java/com/ecjtaneo/hotel_management_system/room/RoomService.java
+++ src/main/java/com/ecjtaneo/hotel_management_system/room/RoomService.java
@@ -1,4 +1,24 @@
 package com.ecjtaneo.hotel_management_system.room;
 
+import com.ecjtaneo.hotel_management_system.common.exception.ResourceNotFoundException;
+import com.ecjtaneo.hotel_management_system.room.model.Room;
+import org.springframework.transaction.annotation.Transactional;
+
 public class RoomService {
+    private RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public Room findByRoomNumber(String roomNumber) {
+        return roomRepository.findByRoomNumber(roomNumber)
+                .orElseThrow(() -> new ResourceNotFoundException("Room not found"));
+    }
+
+    @Transactional
+    public void deleteByRoomNumber(String roomNumber) {
+        roomRepository.deleteByRoomNumber(roomNumber);
+    }
+
 }
