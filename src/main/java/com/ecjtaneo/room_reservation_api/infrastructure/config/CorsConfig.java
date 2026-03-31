bf2fd911f819095f18db69120615a1b68c16cc7f
EcjTn <ecjtaneo@gmail.com>
2026-03-31 09:46:06 +0800

    update cors config

--- src/main/java/com/ecjtaneo/room_reservation_api/infrastructure/config/CorsConfig.java
+++ src/main/java/com/ecjtaneo/room_reservation_api/infrastructure/config/CorsConfig.java
@@ -6,10 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry
                 .addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
